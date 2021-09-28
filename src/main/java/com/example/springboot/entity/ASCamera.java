1898fe0b07fe1562ad396523f9069719b8521a39
vicenfy <srtrack@outlook.com>
2021-09-29 01:52:37 +0200

    add property for parent and children class

--- src/main/java/com/example/springboot/entity/ASCamera.java
+++ src/main/java/com/example/springboot/entity/ASCamera.java
@@ -8,12 +8,209 @@ public class ASCamera extends Camera {
     public ASCamera() {
         super();
     }
+//#sensor#Sensor type#Pixel size#Sensor size#Dynamic range#SNR#Gain#Exposure time#Pixel format#Image buffer#Power consumption#Ingress protection#Compatibility#Operating system  这块AC和LC相机都有
+//#Max. Frame Rate#Shutter mode#Binning#Decimation#Reverse image 特有
+    @Column(name = "sensor")
+    private String sensor;
+
+    @Column(name = "sensor_type")
+    private String sensorType;
+
+    @Column(name = "pixel_size")
+    private String pixelSize;
+
+    @Column(name = "sensor_size")
+    private String sensorSize;
+
+    @Column(name = "dynamic_range")
+    private String dynamicRange;
+
+    @Column(name = "snr")
+    private String snr;
+
+    @Column(name = "gain")
+    private String gain;
+
+    @Column(name = "exposure_time")
+    private String exposureTime;
+
+    @Column(name = "pixel_format")
+    private String pixelFormat;
+
+    @Column(name = "image_buffer")
+    private String imageBuffer;
+
+    @Column(name = "power_consumption")
+    private String powerConsumption;
+
+    @Column(name = "ingress_protection")
+    private String ingressProtection;
+
+    @Column(name = "compatibility")
+    private String compatibility;
 
     @Column(name = "operation_system")
     private String operationSystem;
 
-    @Column(name = "certification")
-    private String certification;
+//    特有
+    @Column(name = "max_frame_rate")
+    private String maxFrameRate;
+
+    @Column(name = "shutter_mode")
+    private String shutterMode;
+
+    @Column(name = "binning")
+    private String binning;
+
+    @Column(name = "decimation")
+    private String decimation;
+
+    @Column(name = "reverse_image")
+    private String reverseImage;
+
+    public String getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(String sensor) {
+        this.sensor = sensor;
+    }
+
+    public String getSensorType() {
+        return sensorType;
+    }
+
+    public void setSensorType(String sensorType) {
+        this.sensorType = sensorType;
+    }
+
+    public String getPixelSize() {
+        return pixelSize;
+    }
+
+    public void setPixelSize(String pixelSize) {
+        this.pixelSize = pixelSize;
+    }
+
+    public String getSensorSize() {
+        return sensorSize;
+    }
+
+    public void setSensorSize(String sensorSize) {
+        this.sensorSize = sensorSize;
+    }
+
+    public String getDynamicRange() {
+        return dynamicRange;
+    }
+
+    public void setDynamicRange(String dynamicRange) {
+        this.dynamicRange = dynamicRange;
+    }
+
+    public String getSnr() {
+        return snr;
+    }
+
+    public void setSnr(String snr) {
+        this.snr = snr;
+    }
+
+    public String getGain() {
+        return gain;
+    }
+
+    public void setGain(String gain) {
+        this.gain = gain;
+    }
+
+    public String getExposureTime() {
+        return exposureTime;
+    }
+
+    public void setExposureTime(String exposureTime) {
+        this.exposureTime = exposureTime;
+    }
+
+    public String getPixelFormat() {
+        return pixelFormat;
+    }
+
+    public void setPixelFormat(String pixelFormat) {
+        this.pixelFormat = pixelFormat;
+    }
+
+    public String getImageBuffer() {
+        return imageBuffer;
+    }
+
+    public void setImageBuffer(String imageBuffer) {
+        this.imageBuffer = imageBuffer;
+    }
+
+    public String getPowerConsumption() {
+        return powerConsumption;
+    }
+
+    public void setPowerConsumption(String powerConsumption) {
+        this.powerConsumption = powerConsumption;
+    }
+
+    public String getIngressProtection() {
+        return ingressProtection;
+    }
+
+    public void setIngressProtection(String ingressProtection) {
+        this.ingressProtection = ingressProtection;
+    }
+
+    public String getCompatibility() {
+        return compatibility;
+    }
+
+    public void setCompatibility(String compatibility) {
+        this.compatibility = compatibility;
+    }
+
+    public String getMaxFrameRate() {
+        return maxFrameRate;
+    }
+
+    public void setMaxFrameRate(String maxFrameRate) {
+        this.maxFrameRate = maxFrameRate;
+    }
+
+    public String getShutterMode() {
+        return shutterMode;
+    }
+
+    public void setShutterMode(String shutterMode) {
+        this.shutterMode = shutterMode;
+    }
+
+    public String getBinning() {
+        return binning;
+    }
+
+    public void setBinning(String binning) {
+        this.binning = binning;
+    }
+
+    public String getDecimation() {
+        return decimation;
+    }
+
+    public void setDecimation(String decimation) {
+        this.decimation = decimation;
+    }
+
+    public String getReverseImage() {
+        return reverseImage;
+    }
+
+    public void setReverseImage(String reverseImage) {
+        this.reverseImage = reverseImage;
+    }
 
     public String getOperationSystem() {
         return operationSystem;
@@ -23,11 +220,4 @@ public class ASCamera extends Camera {
         this.operationSystem = operationSystem;
     }
 
-    public String getCertification() {
-        return certification;
-    }
-
-    public void setCertification(String certification) {
-        this.certification = certification;
-    }
 }
--- src/main/java/com/example/springboot/entity/Camera.java
+++ src/main/java/com/example/springboot/entity/Camera.java
@@ -2,6 +2,7 @@ package com.example.springboot.entity;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -34,6 +35,61 @@ public class Camera {
     @NotNull
     @Column(columnDefinition="TEXT", name = "product_introduction")
     private String productIntroduction;
+
+//Type#resolution#Data Interface#MonoColor#Digital I/O#Power supply#Lens Interface#Dimension#Weight#Temperature#Humidity#Client software#Certification
+//  Type:像素（什么类型的相机）
+    @NotNull
+    @Column(name = "type")
+    private String type;
+
+    @NotNull
+    @Column(name = "resolution")
+    private String resolution;
+
+    //接口
+    @NotNull
+    @Column(name = "data_interface")
+    private String dataInterface;
+
+    @NotNull
+    @Column(name = "mono_color")
+    private String monoColor;
+
+    @NotNull
+    @Column(name = "digital_io")
+    private String digitalIO;
+
+    @NotNull
+    @Column(name = "power_supply")
+    private String powerSupply;
+
+    @NotNull
+    @Column(name = "lens_interface")
+    private String lensInterface;
+
+    @NotNull
+    @Column(name = "dimension")
+    private String dimension;
+
+    @NotNull
+    @Column(name = "weight")
+    private String weight;
+
+    @NotNull
+    @Column(name = "temperature")
+    private String temperature;
+
+    @NotNull
+    @Column(name = "humidity")
+    private String humidity;
+
+    @NotNull
+    @Column(name = "client_software")
+    private String clientSoftware;
+
+    @NotNull
+    @Column(name = "certification")
+    private String certification;
 
     @CreationTimestamp
     private Timestamp createdAt;
@@ -92,5 +148,109 @@ public class Camera {
 
     public void setProductType(String productType) {
         this.productType = productType;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(String resolution) {
+        this.resolution = resolution;
+    }
+
+    public String getDataInterface() {
+        return dataInterface;
+    }
+
+    public void setDataInterface(String dataInterface) {
+        this.dataInterface = dataInterface;
+    }
+
+    public String getMonoColor() {
+        return monoColor;
+    }
+
+    public void setMonoColor(String monoColor) {
+        this.monoColor = monoColor;
+    }
+
+    public String getDigitalIO() {
+        return digitalIO;
+    }
+
+    public void setDigitalIO(String digitalIO) {
+        this.digitalIO = digitalIO;
+    }
+
+    public String getPowerSupply() {
+        return powerSupply;
+    }
+
+    public void setPowerSupply(String powerSupply) {
+        this.powerSupply = powerSupply;
+    }
+
+    public String getLensInterface() {
+        return lensInterface;
+    }
+
+    public void setLensInterface(String lensInterface) {
+        this.lensInterface = lensInterface;
+    }
+
+    public String getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(String dimension) {
+        this.dimension = dimension;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+
+    public String getClientSoftware() {
+        return clientSoftware;
+    }
+
+    public void setClientSoftware(String clientSoftware) {
+        this.clientSoftware = clientSoftware;
+    }
+
+    public String getCertification() {
+        return certification;
+    }
+
+    public void setCertification(String certification) {
+        this.certification = certification;
     }
 }
