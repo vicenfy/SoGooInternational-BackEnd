14f5f6d0e2e1602cf2920c931830eb740d943530
sxu <laozishixs@gmail.com>
2021-09-28 22:44:44 +0200

    Clean up the old classes

--- src/main/java/com/example/springboot/entity/Camera.java
+++ src/main/java/com/example/springboot/entity/Camera.java
@@ -1,13 +1,21 @@
 package com.example.springboot.entity;
 
+import com.example.springboot.exception.ResourceNotFoundException;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.persistence.*;
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.sql.Timestamp;
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Entity
 @Table(name = "odf_sgi_cameras")
@@ -37,6 +45,27 @@ public class Camera {
 
     @UpdateTimestamp
     private Timestamp updatedAt;
+
+//    // Update smart phone by id
+//    @PutMapping("smartphones/{id}")
+//    public ResponseEntity<SmartPhone> updateSmartPhone(@PathVariable(value = "id") long smartPhoneId, @Valid @RequestBody SmartPhone updateSmartPhone) {
+//        SmartPhone smartPhone = smartPhoneRepository.findById(smartPhoneId).orElseThrow(() -> new ResourceNotFoundException("Smart phone not found"));
+//        smartPhone.setName(updateSmartPhone.getName());
+//        smartPhone.setPrice(updateSmartPhone.getPrice());
+//        smartPhone.setStatus(updateSmartPhone.isStatus());
+//        SmartPhone editSmartPhone = smartPhoneRepository.save(smartPhone);
+//        return ResponseEntity.ok().body(editSmartPhone);
+//    };
+//
+//    // Delete smart phone by id
+//    @DeleteMapping("smartphones/{id}")
+//    public Map<String, Boolean> deleteSmartPhone(@PathVariable(value = "id") long smartPhoneId) {
+//        SmartPhone smartPhone = smartPhoneRepository.findById(smartPhoneId).orElseThrow(() -> new ResourceNotFoundException("Smart phone not found"));
+//        this.smartPhoneRepository.delete(smartPhone);
+//        Map<String, Boolean> respone = new HashMap<>();
+//        respone.put("deleted: ", Boolean.TRUE);
+//        return respone;
+//    };
 
     public Long getId() {
         return id;
